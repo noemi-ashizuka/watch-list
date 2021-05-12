22020a9c8afcfc74ac8bc59e46d400493773af4f
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-05-12 19:00:11 +0900

    added bookmark create

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,18 @@
 class BookmarksController < ApplicationController
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show'
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Movie.destroy_all
 
 url = 'http://tmdb.lewagon.com/movie/top_rated'
 
-json_doc = JSON.parse((RestClient.get(url)))
+json_doc = JSON.parse(RestClient.get(url))
 
 movies = json_doc['results']
 
@@ -12,8 +12,10 @@ movies.take(20).each do |movie|
   new_movie = Movie.create!(
     title: movie['title'],
     overview: movie['overview'],
-    poster_url: movie['poster_path'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",
     rating: movie['vote_average']
   )
-  p new_movie.title
+  p "Creating #{new_movie.title}"
 end
+
+p "There are #{Movie.count} movies in the database"
