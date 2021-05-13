2182552248281bbb8f5b14159a4c625ef7bafae5
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-05-13 12:08:19 +0900

    colored rating span

---
+++ app/javascript/packs/color_rating.js
@@ -0,0 +1,14 @@
+function colorRating() {
+  const ratings = document.querySelectorAll(".rating");
+  ratings.forEach((rating) => {
+    if(rating.innerText >= 8) {
+      rating.classList.add('green')
+    } else if(rating >= 5) {
+      rating.classList.add('orange')
+    } else {
+      rating.classList.add('red')
+    }
+  });
+};
+
+colorRating();
