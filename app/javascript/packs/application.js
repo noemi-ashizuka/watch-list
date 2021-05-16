b2332a66faf106c245a63e4abcdc4410c893d5b8
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-05-16 22:07:24 +0900

    worked on index and list form styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
+import "bootstrap"
 
 Rails.start()
 ActiveStorage.start()
